9d49b13f1eb9878a053d3df27980c3bc89e97550
Meghan Carter <msc2010@yahoo.com>
2023-05-28 07:14:54 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const animalSounds = {
+  moo: "cow",
+  neigh: "horse",
+  baa: "sheep",
+  oink: "pig",
+  cluck: "chicken"
+};
+const{moo, neigh, baa, oink, cluck} = animalSounds;
+
+const animalNames = {
+  bessie: "cow",
+  dolly: "sheep",
+  babe: "pig",
+  little: "chicken"
+};
+
+const{bessie, dolly, babe, little} = animalNames;
+
+const animalColors = {
+  blackAndWhite: "cow",
+  black: "sheep",
+  pink: "pig"
+};
+
+const{blackAndWhite, black, pink} = animalColors;
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const[red, orange, yellow, green, blue, indigo, violet] = colors;
+
+const[r, o, y, g, b, i, v] = colors;
+
+const[,,,,, indg,] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +41,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const{muppetName, color, song, job, partner} = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +58,9 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const{song2, song4} = nestedMuppet.album.theMuppetMovie;
+const{nestedJob, nestedPartner} = nestedMuppet;
 
 // Strings
 
